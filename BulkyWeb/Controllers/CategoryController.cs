f235178b1095d7711e411d2dbf667c37c166961d
Innocent Sibiya <innoh.cbia@gmail.com>
2023-05-11 11:59:55 +0200

    add validation

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,5 +17,27 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+           /* if(obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name","The Display Order cannot exactly match the Name");
+            }*/
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
     }
 }

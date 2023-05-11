fbbebcba87ee928411a86c3394d096c809042c37
Innocent Sibiya <innoh.cbia@gmail.com>
2023-05-11 12:25:51 +0200

    add edit and delete buttons on category view

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -26,10 +26,10 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-           /* if(obj.Name == obj.DisplayOrder.ToString()) 
+            if(obj.Name == obj.DisplayOrder.ToString()) 
             {
                 ModelState.AddModelError("name","The Display Order cannot exactly match the Name");
-            }*/
+            }
 
             if (ModelState.IsValid)
             {

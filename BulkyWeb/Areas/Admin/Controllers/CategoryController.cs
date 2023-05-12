6db35008ed493f33136cb33c5dd9af648db137f0
Innocent Sibiya <innoh.cbia@gmail.com>
2023-05-12 12:23:18 +0200

    Add CRUD views and cater for them on the controller

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -3,15 +3,16 @@ using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyWeb.Controllers
+namespace BulkyWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
         public CategoryController(IUnitOfWork db)
         {
             _unitOfWork = db;
-            
+
         }
         public IActionResult Index()
         {
@@ -19,7 +20,7 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
-        public IActionResult Create() 
+        public IActionResult Create()
         {
             return View();
         }
@@ -27,9 +28,9 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString()) 
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("name","The Display Order cannot exactly match the Name");
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
             }
 
             if (ModelState.IsValid)
@@ -43,15 +44,15 @@ namespace BulkyWeb.Controllers
         }
         public IActionResult Edit(int? id)
         {
-            if (id == null || id==0) 
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
-            Category? categoryFromDB = _unitOfWork.Category.GetFirstOrDefault(x=>x.Id == id);
+            Category? categoryFromDB = _unitOfWork.Category.GetFirstOrDefault(x => x.Id == id);
             //Category? categoryFromDB1 = _db.Categories.FirstOrDefault(x=>x.Id == id)
             //Category? categoryFromDB2 = _db.Categories.Where(x=>x.Id == id).FirstOrDefault();
 
-            if (categoryFromDB == null) 
+            if (categoryFromDB == null)
             {
                 return NotFound();
             }
@@ -78,7 +79,7 @@ namespace BulkyWeb.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDB = _unitOfWork.Category.GetFirstOrDefault(x=> x.Id == id);
+            Category? categoryFromDB = _unitOfWork.Category.GetFirstOrDefault(x => x.Id == id);
 
             if (categoryFromDB == null)
             {
@@ -88,7 +89,7 @@ namespace BulkyWeb.Controllers
             return View(categoryFromDB);
         }
 
-        [HttpPost,ActionName("Delete")]
+        [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
             Category? obj = _unitOfWork.Category.GetFirstOrDefault(x => x.Id == id);
